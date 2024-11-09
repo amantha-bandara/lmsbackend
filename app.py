9439b20955fa2f8e66370905739e3097d33bfd64
amantha-bandara <amanthabandara14@gmail.com>
2024-11-09 00:08:57 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, flash,redirect,url_for,session
+from flask import Flask, request, render_template, flash,get_flashed_messages,redirect,url_for,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin,LoginManager,login_user,login_required,logout_user,current_user
 from flask_bcrypt import Bcrypt
+
 
 
 
@@ -13,6 +14,16 @@ app.config['SECRET_KEY'] = 'gbjwk34tjkb!@#$%^&*()6328964789tjkvgabsbgwegbkbdk!(%
 db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    
+    return User.query.get(int(user_id))
+
       
 
 class User(db.Model,UserMixin):
@@ -24,10 +35,10 @@ class User(db.Model,UserMixin):
       
 @app.route('/')
 def ma():
+    flash('hello')
+    
     return render_template('main.html')
-@app.route('/login')
-def login():
-    return render_template('login.html')
+
 
 @app.route('/register',methods =['GET','POST'])
 def reg():
@@ -45,16 +56,41 @@ def reg():
                 user = User(first_name=fm,last_name=lm,email=em,password =hp2)
                 db.session.add(user)
                 db.session.commit()
-                return redirect(url_for('ma'))
+                return redirect(url_for('login'))
             else:
                 flash('email already taken')
                 return render_template('register.html')
         else:
-            flash('somthing wrong maybe ')
+            flash('somthing wrong maybe password')
             return render_template('register.html')
     else:
         return render_template('register.html')
 
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+          ema = request.form['email']
+          pas = request.form['password']
+        
+          if ema and pas:
+            user = User.query.filter_by(email=ema).first()
+            if user:
+                if bcrypt.check_password_hash(user.password,pas):
+                    login_user(user)
+                    flash('login succesful')
+                    #session['email'] = ema
+                    #session['password'] = pas
+                    return render_template('home.html')
+                else:
+                    return render_template('login.html')
+            else:
+               flash('user does not exist')
+               return render_template('login.html')
+          else:
+            flash('fill the form')
+            return render_template('login.html')   
+    else:
+        return render_template('login.html')
 
 
 
@@ -73,6 +109,10 @@ def reg():
 
 
 
+@app.cli.command('create_db')
+def create_db():
+    with app.app_context():
+        db.create_all()
 
 
 
